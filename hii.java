c8a45d092682c98858a7ad3e596e7cc60ccb5bd2
Rittik Saxsena <rittiksaxsena29@gmail.com>
2022-06-03 00:16:18 +0530

    changes in hii.java after github success

--- hii.java
+++ hii.java
@@ -2,5 +2,6 @@ public class hii
 {
     public static void main(String[] args) {
         System.out.println("hii new");
+        System.out.println("tere bhai ne push krdiya github par.....");
     }
 }
